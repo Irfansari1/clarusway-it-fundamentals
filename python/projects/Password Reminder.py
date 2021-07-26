9c0409939d086b966f80b97be551a3ecdbdf357b
irfan <irfansari42@gmail.com>
2021-07-26 10:55:11 +0200

    password reminder eklendi

---
+++ python/projects/Password Reminder.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 12 23:42:14 2021
+
+@author: admin
+"""
+
+password = 12345
+source_name = "irfan"
+
+name= input("please enter your name: ")
+
+if name == source_name:
+    print(f"Hello {name}! The password is {password}")
+    
+else: 
+    print(f"Hello {name}! See you later")
+    
+
+
