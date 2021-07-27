4ff79e6073513ec53e3c64c313adf7e5bc3c7eb7
irfan <irfansari42@gmail.com>
2021-07-27 10:46:05 +0200

    Measure Converter eklendi

---
+++ python/projects/Measure Converter.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 22 11:49:24 2021
+
+@author: admin
+"""
+
+celcius = int(input("Please enter the temperature in Celcius"))
+
+fahrenheit = (celcius * 9/5) + 32
+
+print("The converted temperature is", fahrenheit)
+
+
+
+
+
+km = int(input("Please enter the distance in km"))
+
+mile = (km * 0.6)
+
+print("The converted distance is", mile, "miles")
